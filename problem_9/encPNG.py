3273171daf91a01da1087d3dde92f57af71779c5
robert <sjia0385@uni.sydney.edu.au>
2020-10-12 01:24:30 +1100

    code comment fix, ECB=trash :(

--- problem_9/encPNG.py
+++ problem_9/encPNG.py
@@ -4,17 +4,14 @@ from Cryptodome.Util.Padding import pad
 # get the binary png file
 pic = open("myfile.png.bin","rb")
 
-# encode the png file to bytes of data
+# read the png file as bytes of data
 data = pic.read()
 
 # encode the key
 key = "INFO3616INFO3616".encode('utf-8')
 
-# create a ECB mode block cipher encryption with random 16-bytes IV to XOR with png data
+# create an ECB mode block cipher encryption
 cipher = AES.new(key, AES.MODE_ECB)
-
-# this is the iv used
-# iv = b64encode(cipher.iv).decode('utf-8')
 
 # encrypt the data with 16-byte block size and pad the remainder with 0
 ct_bytes = cipher.encrypt(pad(data, AES.block_size))
