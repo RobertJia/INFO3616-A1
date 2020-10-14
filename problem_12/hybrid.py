c4824d13a4e5ddcb9a010ff4e59312d214f3d134
RobertJia <sjia0385@uni.sydney.edu.au>
2020-10-15 03:19:54 +1100

    p12 code comment fix

--- problem_12/hybrid.py
+++ problem_12/hybrid.py
@@ -1,34 +1,28 @@
-from Cryptodome.Util.Padding import pad, unpad
 from Cryptodome.Cipher import AES
-from base64 import b64encode, b64decode
-from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Util.Padding import pad, unpad
 from Cryptodome.Random import get_random_bytes
+from base64 import b64encode, b64decode
+
 class Principal:
 
     # key_length: RSA key length in bits this principal will use
     # name: name of principal, save key under "name".der in DER format
     def __init__(self, key_length, name):
-        # YOUR TASK STARTS HERE
         self.name = name
         self.key_length = key_length
         self.own_key = self.create_rsa_key(key_length)
-        # YOUR TASK ENDS HERE
         with open("{}.der".format(name), "wb") as out_fh:
             out_fh.write(self.own_key.exportKey(format ='DER', pkcs=1))
 
     # Create RSA key of given key_length
     def create_rsa_key(self, key_length):
-        # YOUR TASK STARTS HERE
         rsa_keypair = RSA.generate(key_length)
-        # YOUR TASK ENDS HERE
         return rsa_keypair
 
     # Return public key part of public/private key pair
     def get_public_key(self):
-        # YOUR TASK STARTS HERE
-        # nothing to do here
-        # YOUR TASK ENDS HERE
         public_key = self.own_key.publickey()
         return public_key
 
@@ -36,14 +30,12 @@ class Principal:
     # Returns: encrypted key (bytes), encrypted message (bytes), IV (bytes),
     # number of padding bytes
     def receive(self, filename):
-        # YOUR TASK STARTS HERE
         encfile = open(filename, 'r')
         alllines = encfile.readlines()
         ck_bytes = bytes.fromhex(alllines[0].rstrip("\n"))
         cm_bytes = bytes.fromhex(alllines[1].rstrip("\n"))
         iv_bytes = bytes.fromhex(alllines[2].rstrip("\n"))
         pad_len_int = int(alllines[3].rstrip("\n"))
-        # YOUR TASK ENDS HERE
         return [ck_bytes, cm_bytes, iv_bytes, pad_len_int]
 
     # Sending means writing an encrypted message plus metadata to a file.
@@ -52,8 +44,6 @@ class Principal:
     # Line 3: IV as hex string
     # Line 4: Number of padding bytes (string of int)
     def send(self, filename, msg):
-        # YOUR TASK STARTS HERE
-        # ...
         encfile = open(filename, 'w')
         ck_hex = msg[0]
         cm_hex = msg[1]
@@ -61,8 +51,7 @@ class Principal:
         pad_len_hex = msg[3]
         encfile.write('\n'.join([ck_hex, cm_hex, iv_hex, str(pad_len_hex)]))
         encfile.close()
-        # YOUR TASK ENDS HERE
-        pass
+
 
 # Hybrid Cipher encapsulates the functionality of a hybrid cipher using
 # RSA and AES-CBC.
@@ -73,55 +62,48 @@ class HybridCipher:
     # own_key: public/private key pair of owner (principal who can decrypt)
     # remote_pub_key: public key of principal this hybrid cipher is encrypting to
     def __init__(self, length_sym, own_key, remote_pub_key):
-        # YOUR TASK STARTS HERE
-        # ...
         self.length_sym = length_sym
         self.own_key = own_key
         self.remote_pub_key = remote_pub_key
-        # YOUR TASK ENDS HERE
-        pass
-
 
     # Creates an AES cipher in CBC mode with random IV, and random key
-    # Returns: cipher, IV, symmetric key
+    # Returns: cipher, IV (bytes), symmetric key (bytes)
     def create_aes_cipher(self, length):
-        # YOUR TASK STARTS HERE
-        # get the key with specified length, randomly
+        # get the key with specified length in bits as a byte string, randomly
         sym_key = get_random_bytes(int(length/8))
         # create a CBC mode block cipher encryption with random IV to XOR with the plain text data
         cipher = AES.new(sym_key, AES.MODE_CBC)
         # this is the iv used, encoded in base 64
         iv = b64encode(cipher.iv)
-        # YOUR TASK ENDS HERE
         return cipher, iv, sym_key
-
 
     # Decrypted hybrid-encrypted msg (list of bytes received)
     # Returns: decrypted message with padding removed, as string
     def decrypt(self, msg):
-        # YOUR TASK STARTS HERE
         ck_bytes = msg[0]
         cm_bytes = msg[1]
         iv_bytes = msg[2]
+        # create a PKCS1_OAEP cipher with Bob's own key pair for RSA decryption
         RSAcipher = PKCS1_OAEP.new(self.own_key)
+        # decrypt the symmetric key using Bob's private key
         sym_key = RSAcipher.decrypt(ck_bytes)
+        # create an AES cipher in CBC mode with received iv and the symmetric key decrypted
         decipher = AES.new(sym_key, AES.MODE_CBC, b64decode(iv_bytes))
+        # decrypt and unpad the message using the symmetric key        
         rcvd_msg_dec = unpad(decipher.decrypt(cm_bytes), AES.block_size).decode()
-        # YOUR TASK ENDS HERE
         return rcvd_msg_dec
-
 
     # remote_pub_key: key pair, sym_key: bytes
     def encrypt_smkey(self, remote_pub_k, sym_key):
+        # create a PKCS1_OAEP cipher with Bob's public key for RSA encryption
         RSAcipher = PKCS1_OAEP.new(remote_pub_k)
+        # encrypt the symmetric key using Bob public key
         enc_sym_key = RSAcipher.encrypt(sym_key)
         return enc_sym_key
-    
 
     # Encrypts plaintext msg (string) to encrypt in hybrid fashion.
-    # Returns: encrypted symmetric key (hex), encrypted message (hex), IV (hex), number of padding bytes
+    # Returns: encrypted symmetric key (hex), encrypted message (hex), IV (hex), number of padding bytes (int)
     def encrypt(self, msg):
-        # YOUR TASK STARTS HERE
         # get the cipher, IV and symmetric key
         cipher_tuple = self.create_aes_cipher(self.length_sym)
         cipher = cipher_tuple[0]
@@ -132,28 +114,21 @@ class HybridCipher:
         cm = ct_bytes.hex()
         ck = self.encrypt_smkey(self.remote_pub_key, sym_key).hex()
         pad_len = (AES.block_size - len(msg)) % AES.block_size
-        # YOUR TASK ENDS HERE
         return [ck, cm, iv, pad_len]
 
     # Padding for AES-CBC.
     # Pad up to multiple of block length by adding 0s (as byte)
     # Returns: padded message, number of padding bytes
     def pad(self, msg):
-        # YOUR TASK STARTS HERE
         # not using
         padded_msg = ""
-        # YOUR TASK ENDS HERE
         return padded_msg
 
     # Strips padding and converts message to str.
     def strip_pad(self, msg, pad_len_int):
-        # YOUR TASK STARTS HERE
         # not using
         msg_unpadded = ""
-        # YOUR TASK ENDS HERE
         return msg_unpadded
-
-
 
 
 def main():
@@ -169,7 +144,7 @@ def main():
     a_hybrid_cipher = HybridCipher(256, alice.own_key, bob.get_public_key())
 
     # Alice has a message for Bob.
-    msg = "Hi Bob, it's Alice."
+    msg = "Hi Bob, it's Alice.\nWe are using hybrid encryption to communicate."
     # Alice uses the hybrid cipher to encrypt to Bob.
     msg_enc = a_hybrid_cipher.encrypt(msg)
     alice.send("msg.enc", msg_enc)
